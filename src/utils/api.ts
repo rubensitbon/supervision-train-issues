e4412b350b9e6c398c2fabb09b492498276b5cb6
Ruben Sitbon <ruben.sitbon@outlook.com>
2020-02-12 15:21:09 +0100

    Add Env files

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,15 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://192.168.43.137:3000';
+console.log('process.env.REACT_APP_SERVER_IP', process.env.REACT_APP_SERVER_IP);
+console.log(
+  'process.env.REACT_APP_SERVER_PORT',
+  process.env.REACT_APP_SERVER_PORT
+);
+
+export const BASE_URL =
+  process.env.REACT_APP_SERVER_IP && process.env.REACT_APP_SERVER_PORT
+    ? `http;//${process.env.REACT_APP_SERVER_IP}:${process.env.REACT_APP_SERVER_PORT}`
+    : 'http://localhost:3000';
 
 const api = axios.create({
   baseURL: BASE_URL
